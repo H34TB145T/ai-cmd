e3ef776a7fa690447bd3e534419c4ca76b555008
YeminthanRiki <157350470+H34TB145T@users.noreply.github.com>
2025-07-19 01:18:23 +0700

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 echo "🧹 Uninstalling AI CMD..."
 sudo rm -f /usr/local/bin/aicmd
-sudo rm -f /usr/local/bin/aicmd-run
 rm -f ~/.aicmdkey
 echo "✅ Uninstalled successfully."
 echo ":(( Didn't you like it? You can always create an issue! --> https://github.com/H34TB145T"
